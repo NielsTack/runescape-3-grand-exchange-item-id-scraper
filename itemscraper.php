c01cbf548b447a0cbdcf22a3cfdd41a3939fa0de
NielsTack <niels102@live.nl>
2021-03-27 14:04:18 +0100

    added looping thought pages

--- itemscraper.php
+++ itemscraper.php
@@ -1,25 +1,40 @@
 <?php
 $alphabetrange = range('a', 'z');
 $categoryrange = range(1,37);
+$pagerange = 1;
 $items = [];
 // loop over category and alphabet to get each item
 foreach ($categoryrange as $category) {
     foreach ($alphabetrange as $alphabet) {
-        try {
-            echo ('parsing https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category='.$category.'&alpha='.$alphabet."\n");
-            $json = file_get_contents('https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category='.$category.'&alpha='.$alphabet);
-            $objects = json_decode($json, true);
-            echo ('succesfully parsed category='.$category.'&alpha='.$alphabet."\n");
-
-            // loop over each item in the page and push it as key value pair to items array
-            foreach ($objects['items'] as $object ) {
-                echo ('added '.$object['name'].' with id '. $object['id']."\n");
-                $items[$object['name']] = $object['id'];
-
+        // also loop over each page
+        $pagerange = 1;
+        $error = false;
+        while($error == false){
+            try {
+                echo ('parsing https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category='.$category.'&alpha='.$alphabet.'&page='.$pagerange."\n\n");
+                $json = file_get_contents('https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category='.$category.'&alpha='.$alphabet.'&page='.$pagerange);
+                $objects = json_decode($json, true);
+                echo ('succesfully parsed category='.$category.'&alpha='.$alphabet.'&page='.$pagerange."\n\n");
+    
+                // loop over each item in the page and push it as key value pair to items array
+                if($objects['items'][0]['name'] != NULL){
+                    foreach ($objects['items'] as $object ) {
+                        echo ('added '.$object['name'].' with id '. $object['id']."\n\n");
+                        $items[$object['name']] = $object['id'];
+                    }
+                    $pagerange++;
+                } else {
+                    $error = true;
+                }
+  
+            } catch (Exception $e) {
+                $error = true;
+                echo 'Caught exception: ',  $e->getMessage(), "\n\n";
+                
             }
-        } catch (Exception $e) {
-            echo 'Caught exception: ',  $e->getMessage(), "\n";
         }
+        
+
     }
 }
 
