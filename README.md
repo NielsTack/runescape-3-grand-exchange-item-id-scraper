ae63f0bf9399e55519065f4dbae81e3f33a8a201
NielsTack <niels102@live.nl>
2021-03-27 18:18:15 +0100

    scraper now loops through pages correctly

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 project that scrapes and stores all item id's from the runescape 3 grand exchange.
 
 runescapes api doesnt give you a list of all item id's so you have to search the manually. Included in this project you will find a json key value pair of all items with its corresponding ID.
+note that due to the nature of the API and the many requests I have to make the process can take up to 2 hours.
--- itemscraper.php
+++ itemscraper.php
@@ -10,45 +10,62 @@ foreach ($categoryrange as $category) {
         $pagerange = 1;
         $error = false;
         while($error == false){
+          
             try {
-                echo ('parsing https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category='.$category.'&alpha='.$alphabet.'&page='.$pagerange."\n\n");
+                $start = microtime(true);
                 $json = file_get_contents('https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category='.$category.'&alpha='.$alphabet.'&page='.$pagerange);
                 $objects = json_decode($json, true);
-                echo ('succesfully parsed category='.$category.'&alpha='.$alphabet.'&page='.$pagerange."\n\n");
-    
+                echo nl2br('succesfully parsed category='.$category.'&alpha='.$alphabet.'&page='.$pagerange."\n");
+                $time_elapsed_secs = microtime(true) - $start;
+
+                // sleep so content has time to load and not too many requests are made too soon after eachother
+                if($time_elapsed_secs < 5){
+                    sleep(5-$time_elapsed_secs);
+                }
+
+                // debugging
+                // echo nl2br($time_elapsed_secs."\n");
+                // echo nl2br($objects['items']."\n");
+                // echo nl2br(gettype($objects['items'])."\n");
+                // echo nl2br(count($objects['items'])."\n");
+
                 // loop over each item in the page and push it as key value pair to items array
-                if($objects['items'][0]['name'] != NULL){
+                if(count($objects['items']) != '0'){
                     foreach ($objects['items'] as $object ) {
-                        echo ('added '.$object['name'].' with id '. $object['id']."\n\n");
+                        echo nl2br('added '.$object['name'].' with id '. $object['id']."\n");
                         $items[$object['name']] = $object['id'];
                     }
                     $pagerange++;
                 } else {
                     $error = true;
                 }
+
   
             } catch (Exception $e) {
                 $error = true;
-                echo 'Caught exception: ',  $e->getMessage(), "\n\n";
                 
             }
+
         }
         
 
     }
 }
 
+
+
 // encode items array to json
 $json = json_encode($items);
 
 //write json to file
 if (file_put_contents("items.json", $json)) {
-    echo "JSON file created successfully...\n\n";
+    echo nl2br("JSON file created successfully...\n");
 } else {
-    echo "Oops! Error creating json file...\n\n";
+    echo nl2br("Oops! Error creating json file...\n");
 }
 
 // in case writing to file didnt work for some reason, echo items so you dont have to start over
 echo $json
+
 
 ?>
